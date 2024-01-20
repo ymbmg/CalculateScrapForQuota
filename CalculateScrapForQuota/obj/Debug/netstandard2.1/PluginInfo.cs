42a571879f51e76a1b94fc534fb541c20ffbd6ca
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-01-20 02:07:56 -0500

    project/solution renaming
    LethalCompanyTemplate to CalculateScrapForQuota

---
+++ CalculateScrapForQuota/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace CalculateScrapForQuota
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "CalculateScrapForQuota";
+        public const string PLUGIN_NAME = "CalculateScrapForQuota";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# LethalCompanyTemplate
+# CalculateScrapForQuota
 A template for a BepInEx 5 Plugin, using the correct .csproj configuration for Lethal Company modding
