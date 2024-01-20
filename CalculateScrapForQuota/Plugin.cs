f59c3405ecf9e9ee724cdcf51f69b2eebd2d1ac9
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-01-20 02:58:55 -0500

    Update Plugin.cs

--- CalculateScrapForQuota/Plugin.cs
+++ CalculateScrapForQuota/Plugin.cs
@@ -1,14 +1,29 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
 
 namespace CalculateScrapForQuota
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
+        internal static ManualLogSource Log;
+        
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            
+            Log = Logger;
+            
+            Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
