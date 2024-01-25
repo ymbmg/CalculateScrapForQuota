890542b27c2af595191aeb7110f0e431c3c1efb5
JazzyLucas <59397873+JazzyLucas@users.noreply.github.com>
2024-01-24 21:33:29 -0500

    added Unit Testing + better MathUtil

--- CalculateScrapForQuota/Utils/MathUtil.cs
+++ CalculateScrapForQuota/Utils/MathUtil.cs
@@ -1,24 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CalculateScrapForQuota.Utils;
 
 public class MathUtil
 {
-    public static (List<T> combination, int totalValue) FindBestCombination<T>(List<T> items, int quota, Func<T, int> value)
+    public static (List<T> combination, int totalValue) FindBestCombination<T>(
+        List<T> items, 
+        int quota, 
+        Func<T, int> valueSelector)
     {
+        if (items == null) throw new ArgumentNullException(nameof(items));
+        if (valueSelector == null) throw new ArgumentNullException(nameof(valueSelector));
+        if (quota < 0) throw new ArgumentException("Quota cannot be negative.", nameof(quota));
+
         int n = items.Count;
-        int[,] dp = new int[n + 1, quota + 1];
+        int maxValue = items.Sum(item => valueSelector(item));
+        int[,] dp = new int[n + 1, maxValue + 1];
 
         for (int i = 0; i <= n; i++)
         {
-            for (int j = 0; j <= quota; j++)
+            for (int j = 0; j <= maxValue; j++)
             {
                 if (i == 0 || j == 0)
                     dp[i, j] = 0;
                 else
                 {
-                    int itemValue = value(items[i - 1]);
+                    int itemValue = valueSelector(items[i - 1]);
                     if (itemValue <= j)
                         dp[i, j] = Math.Max(dp[i - 1, j], dp[i - 1, j - itemValue] + itemValue);
                     else
@@ -27,21 +36,30 @@ public class MathUtil
             }
         }
 
-        // Backtrack to find the items
-        var result = new List<T>();
-        int res = dp[n, quota];
-        int w = quota;
-        for (int i = n; i > 0 && res > 0; i--)
+        // Find the smallest total value greater than or equal to the quota
+        int totalValue = 0;
+        for (int j = quota; j <= maxValue; j++)
         {
-            if (res != dp[i - 1, w])
+            if (dp[n, j] >= quota)
             {
-                result.Add(items[i - 1]);
-                res -= value(items[i - 1]);
-                w -= value(items[i - 1]);
+                totalValue = j;
+                break;
             }
         }
 
-        int totalValue = dp[n, quota];
+        // Backtrack to find the items
+        var result = new List<T>();
+        for (int i = n, j = totalValue; i > 0 && j > 0; i--)
+        {
+            if (dp[i, j] != dp[i - 1, j])
+            {
+                result.Add(items[i - 1]);
+                j -= valueSelector(items[i - 1]);
+            }
+        }
+
+        result.Reverse(); // Reverse to get the correct order
         return (result, totalValue);
     }
 }
+
